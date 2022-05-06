6613833c2284f8908b98e5981fa8513588537507
Alberto Capriolo <caa@ti8m.ch>
2022-05-06 19:02:17 +0200

    MaxMin solution

--- src/com/acapriolo6/hackerrank/interview_preparation_kit/greedy_algorithms/minmax/MaxMin.java
+++ src/com/acapriolo6/hackerrank/interview_preparation_kit/greedy_algorithms/minmax/MaxMin.java
@@ -1,16 +1,14 @@
 package com.acapriolo6.hackerrank.interview_preparation_kit.greedy_algorithms.minmax;
 
+import kotlin.Pair;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
@@ -29,35 +27,15 @@ class Result {
     public static int maxMin(int k, List<Integer> arr) {
         // Write your code here
         Collections.sort(arr);
-        Point[] distances = new Point[arr.size()];
-        for (int i = 0; i < arr.size() - 1; i++) {
-            distances[i] = new Point(arr.get(i+1) - arr.get(i), i);
-        }
-        distances[arr.size() - 1] = new Point(null, arr.size() - 1);
-        Arrays.sort(distances, new Comparator<Point>() {
-            @Override
-            public int compare(Point point, Point t1) {
-                if (point.distance == null) {
-                    return 1;
-                }
-                if (t1.distance == null) {
-                    return -1;
-                }
-                return point.distance.compareTo(t1.distance);
-            }
-        });
-        int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < k; i++) {
-            if (arr.get(distances[i].index) > max) {
-                max = arr.get(distances[i].index);
-            }
-            if (arr.get(distances[i].index) < min) {
-                min = arr.get(distances[i].index);
+        for (int i = k - 1; i < arr.size(); i++) {
+            int temp = arr.get(i) - arr.get(i-k+1);
+            if (min > temp) {
+                min = temp;
             }
         }
-
-        return max - min;
+        
+        return min;
     }
 
     static class Point {
